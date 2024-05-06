ec96ff1110a7fa8756905d6ff9b55cd5b873c696
Corey Moore <MOO19008510@btc.ac.uk>
2024-05-06 03:58:22 +0100

    Final Push/ Apperance of website changed

---
+++ Pages/Blog.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ToDoExampleAndy.Pages
+{
+    public class BlogModel : PageModel
+    {
+        private readonly ILogger<BlogModel> _logger;
+
+        public BlogModel(ILogger<BlogModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
